ddd1b4f368bbaa9b6c5e84c7eec8f0028c0bbe92
temmie0232 <temmie0232@gmail.com>
2024-08-15 00:35:53 +0900

    update: 商品タイトルを中央揃え

--- src/app/home/manual/[productID]/page.tsx
+++ src/app/home/manual/[productID]/page.tsx
@@ -73,7 +73,9 @@ const ProductDetailPage: React.FC<ProductDetailPageProps> = ({ params }) => {
     return (
         <Layout>
             <div className="max-w-2xl mx-auto px-8 sm:px-8 lg:px-10">
-                <h1 className="text-3xl font-bold mb-4 justify-items-center">{product.name}</h1>
+                <div className="flex justify-center items-center mb-4">
+                    <h1 className="text-3xl font-bold">{product.name}</h1>
+                </div>
                 <div className="flex justify-center mt-1 mb-8">
                     <div className="w-16 h-1 bg-black rounded-lg"></div>
                 </div>
